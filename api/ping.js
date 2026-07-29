89521e2b9fbe4ea085732408f8308835fab87aa4
David <154765655+Davidhaba@users.noreply.github.com>
2026-07-29 03:07:00 +0200

    Delete api/ping.js

--- api/ping.js
+++
@@ -1,8 +0,0 @@
-module.exports = async (req, res) => {
-  if (req.method !== 'GET') {
-    res.setHeader('Allow', 'GET');
-    return res.status(405).json({ error: 'Method not allowed' });
-  }
-
-  return res.status(200).json({ status: 'ok' });
-};
